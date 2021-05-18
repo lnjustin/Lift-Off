ebd26903723e658ac197b958338f564e0b75a909
lnjustin <lnjustin@users.noreply.github.com>
2021-05-18 17:28:47 -0400

    Update LiftOff.groovy

--- LiftOff.groovy
+++ LiftOff.groovy
@@ -48,7 +48,8 @@ preferences
         input name: "clearWhenInactive", type: "bool", title: "Clear Tile When Inactive?", defaultValue: false
         input name: "hoursInactive", type: "number", title: "Inactivity Threshold (In Hours)", defaultValue: 24
         input name: "refreshInterval", type: "number", title: "Refresh Interval (In Minutes)", defaultValue: 120
-        input name: "showName", type: "bool", title: "Show Launch Name on Tile", defaultValue: false
+        input name: "showName", type: "bool", title: "Show Launch Name on Tile?", defaultValue: false
+        input name: "showLocality", type: "bool", title: "Show Launch Location on Tile?", defaultValue: false
         input name: "textColor", type: "text", title: "Tile Text Color (Hex)", defaultValue: "#000000"
         input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: true
     }
@@ -94,6 +95,7 @@ def updateDevice(data) {
     sendEvent(name: "time", value: data.launch != null ? data.launch.time : "No Launch Data")
     sendEvent(name: "timeStr", value: data.launch != null ? data.launch.timeStr : "No Launch Data")
     sendEvent(name: "name", value: data.launch != null ? data.launch.name : "No Launch Data")
+    sendEvent(name: "location", value: data.launch != null ? data.launch.locality : "No Launch Data")
     def description = ""
     if (data.launch == null) description = "No Launch Data"
     else if (data.launch.description == null) description = "No Description Available"
@@ -140,6 +142,7 @@ def getTile(launch) {
             tile += "<tr><td width='100%' align=center><img src='${launch.patch}' width='100%'></td>"
             if (showName) tile += "<tr style='padding-bottom: 0em'><td width='100%' align=center colspan=3>${launch.name}</td></tr>"
             tile += "<tr style='padding-bottom: 0em'><td width='100%' align=center colspan=3>${launch.timeStr}</td></tr>"
+            if (showLocality) tile += "<tr style='padding-bottom: 0em'><td width='100%' align=center colspan=3>${launch.locality}</td></tr>"
             if (launch.status != "Scheduled" && launch.status != null && launch.status != "null") tile += "<tr style='padding-bottom: 0em'><td width='100%' align=center colspan=3>${launch.status}</td></tr>"
             tile += "</table></div>"  
         }
@@ -183,14 +186,23 @@ def setLatestLaunch() {
     def unixTimestamp = (latest.date_unix as Long) * 1000
     def launchTime = new Date(unixTimestamp)
     def status = latest.success == "true" ? "Success" : "Failure"
-    state.latestLaunch = [time: launchTime.getTime() , timeStr: getTimeStr(launchTime),  name: latest.name, description: latest.details, patch: latest.links.patch.large, status: status]
+    
+    def launchPadID = latest.launchpad
+    def launchPad = httpGetExec("launchpads/" + launchPadID)
+    def locality = launchPad?.locality
+    
+    state.latestLaunch = [time: launchTime.getTime() , timeStr: getTimeStr(launchTime),  name: latest.name, description: latest.details, locality: locality, patch: latest.links.patch.large, status: status]
 }
 
 def setNextLaunch() {
     def next = httpGetExec("launches/next")
     def unixTimestamp = (next.date_unix as Long) * 1000
     def launchTime = new Date(unixTimestamp)
-    state.nextLaunch = [time: launchTime.getTime(), timeStr: getTimeStr(launchTime),  name: next.name, description: next.details, patch: next.links.patch.large, status: "Scheduled"]    
+    
+    def launchPadID = next.launchpad
+    def launchPad = httpGetExec("launchpads/" + launchPadID)
+    def locality = launchPad?.locality
+    state.nextLaunch = [time: launchTime.getTime(), timeStr: getTimeStr(launchTime),  name: next.name, description: next.details, locality: locality, patch: next.links.patch.large, status: "Scheduled"]    
 }
 
 def getLaunchToDisplay() {
